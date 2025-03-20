66fae0c5b059d7230b911fbf4c61eed928a1ba12
Carlos Enrique Cimino <ciminoce@yahoo.com.ar>
2025-03-19 21:43:03 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio01.Consola/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Ejercicio01.Entidades;
+
+try
+{
+	Console.WriteLine("Hello, Calculator!");
+	Console.Write("Ingrese el primer número:");
+	var numero1 = int.Parse(Console.ReadLine()!);
+	Console.Write("Ingrese el segundo número:");
+	var numero2 = int.Parse(Console.ReadLine()!);
+	Calculadora c = new Calculadora(numero1, numero2);
+	Console.WriteLine("0-Salir");
+	Console.WriteLine("1-Sumar");
+	Console.WriteLine("2-Restar");
+	Console.WriteLine("3-Multiplicar");
+	Console.WriteLine("4-Dividr");
+	Console.Write("Ingrese selección:");
+	var opcion = Console.ReadLine();
+	switch (opcion)
+	{
+		case "0":
+			return;
+		case "1":
+			Console.WriteLine(c.Sumar());
+			break;
+		case "2":
+			Console.WriteLine(c.Restar());
+			break;
+		case "3":
+			Console.WriteLine(c.Multiplicar());
+			break;
+		case "4":
+			Console.WriteLine(c.Dividir());
+			break;
+		default:
+			break;
+	}
+
+}
+catch (Exception ex)
+{
+
+    Console.WriteLine(ex.Message); ;
+}
---
+++ Ejercicio01.Entidades/Calculadora.cs
@@ -0,0 +1,36 @@
+﻿namespace Ejercicio01.Entidades
+{
+    public class Calculadora
+    {
+        public Calculadora(int numero1, int numero2)
+        {
+            Numero1 = numero1;
+            Numero2 = numero2;
+        }
+
+        public int Numero1 { get; set; }
+        public int Numero2 { get; set; }
+
+        public int Sumar()
+        {
+            return Numero1 + Numero2;
+        }
+        public int Restar()
+        {
+            return Numero1 - Numero2;
+        }
+        public int Multiplicar()
+        {
+            return Numero1 * Numero2;
+        }
+        public double Dividir()
+        {
+            if (Numero2!=0)
+            {
+                return Numero1 / Numero2;
+
+            }
+            throw new ArgumentException("No se puede dividir por cero",nameof(Numero2));
+        }
+    }
+}
